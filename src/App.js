2a61fd99aeb8562d53c4271d0f8237af3dae98b6
Julian lange <julian.laenge@gmail.com>
2020-12-09 16:40:19 +0200

    Initial commit after some clean-up and "yarn create react-app"

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './images/I-PetsLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="I-PetsLogo" />
+        <h2>
+          Coming Soon...
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
