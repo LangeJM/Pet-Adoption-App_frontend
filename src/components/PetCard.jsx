3d107ae907492a7e2bbab11ae72626c0b9d869bd
Julian lange <julian.laenge@gmail.com>
2020-12-30 09:22:13 +0200

    Minor styling adjustment to add bottom border for pet card images to clearly separate images from body text.

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -7,32 +7,36 @@ import "./PetCard.css";
 
 const PetCard = (props) => {
   function startsWithVowel(word) {
+    // https://stackoverflow.com/a/59523871/12754665
     const vowels = "aeiouAEIOU";
     return vowels.indexOf(word[0]) !== -1;
   }
 
-  let firstCardSentence = "";
-  if (startsWithVowel(props.breed)) firstCardSentence = "is an";
-  else firstCardSentence = "is a";
+  let indefiniteArticle = "";
+  if (startsWithVowel(props.breed)) indefiniteArticle = "an";
+  else indefiniteArticle = "a";
 
   return (
     <div className="mb-4">
-      <Card style={{ width: "18rem" }}>
-        {/* Not sure what the images params are there for and whether they're needed */}
+      <Card style={{ width: "18rem" }} className="overflow-hidden">
         <div
-          style={{ width: "18rem", height: "12rem" }}
+          style={{
+            width: "18rem",
+            height: "12rem",
+            borderBottom: "solid #DFDFDF 1px",
+          }}
           className="overflow-hidden"
         >
           <Card.Img
             variant="top"
-            src={`${props.image}?text=Image cap`}
-            className={"img-fluid"}
+            src={`${props.image}?text=Image of ${indefiniteArticle} ${props.breed}`}
+            className="img-fluid"
           />
         </div>
         <Card.Body className="text-left">
           <Card.Title>{props.name}</Card.Title>
           <Card.Text>
-            {props.name} {firstCardSentence} {props.breed}. <br />
+            {props.name} is {indefiniteArticle} {props.breed}. <br />
             Status: <b>{props.status}</b>.
           </Card.Text>
           <Button className="mt-1 float-right" variant="primary">
